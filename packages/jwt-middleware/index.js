61d8d57dce12cb22f40d2962dea4fdf5e11cb1d0
Geoffrey Goodman <ggoodman@gmail.com>
2018-04-13 11:01:41 -0400

    Simplify middleware to rely on express-jwt built-in token resolution

--- packages/jwt-middleware/index.js
+++ packages/jwt-middleware/index.js
@@ -1,19 +1,15 @@
 'use strict';
 
-const Assert = require('assert');
 const ExpressJwt = require('express-jwt');
 const JwksRsa = require('jwks-rsa');
 
 const WT_AUTH_EXEC = 'wt-authorize-execution';
-const WT_META_EXEC_SCOPE ='wt-execution-scope';
+const WT_META_EXEC_SCOPE = 'wt-execution-scope';
 const WT_META_ADMIN_SCOPE = 'wt:admin';
 const WT_META_ISS = 'wt-execution-iss';
-const WT_META_AUD ='wt-execution-aud';
+const WT_META_AUD = 'wt-execution-aud';
 
-const AUTHZ = 'authorization';
-const BEARER = /^bearer\s+(.+)\s*$/i;
-
-module.exports = function middlewareFactory(){
+module.exports = function middlewareFactory() {
     let cachedMiddlewareInstance;
 
     return function(req, res, next) {
@@ -22,13 +18,12 @@ module.exports = function middlewareFactory(){
         }
 
         return cachedMiddlewareInstance(req, res, next);
-    }
-}
+    };
+};
 
 function createMiddleware(req) {
-
     const iss = req.webtaskContext.meta[WT_META_ISS];
-    const aud = req.webtaskContext.meta[WT_META_AUD]
+    const aud = req.webtaskContext.meta[WT_META_AUD];
 
     // setup JWT middleware
 
@@ -36,79 +31,54 @@ function createMiddleware(req) {
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `${iss}`
+        jwksUri: `${iss}`,
     });
 
     const middleware = ExpressJwt({
         algorithms: ['RS256'],
         audience: aud,
+        credentialsRequired: true,
         issuer: iss,
-        secret: loadRsaKey
+        secret: loadRsaKey,
     });
 
     return function(req, res, next) {
-
         const ctx = req.webtaskContext;
 
         // if wt-authorize-execution is set to true (non-zero) then proceed with authn and authz
 
-        if (ctx.meta[WT_AUTH_EXEC] && ctx.meta[WT_AUTH_EXEC] !== "0"){
+        if (ctx.meta[WT_AUTH_EXEC] && ctx.meta[WT_AUTH_EXEC] !== '0') {
+            return middleware(req, res, error => {
+                if (error) {
+                    let e = new Error(error.name);
+                    e.statusCode = error.status;
+                    return next(e);
+                } else {
+                    // authorization
+                    const scope = !Array.isArray(req.user.scope)
+                        ? req.user.scope.split(' ')
+                        : req.user.scope;
+                    const meta_exec_not_found =
+                        !ctx.meta[WT_META_EXEC_SCOPE] ||
+                        scope.indexOf(ctx.meta[WT_META_EXEC_SCOPE]) === -1;
 
-            if (req.headers[AUTHZ]) {
-
-                // authentication
-
-                try {
-
-                    return middleware(req, res, error => {
-
-                        if (error) {
-
-                            let e = new Error(error.name);
-                            e.statusCode = error.status;
-                            return next(e);
-
-                        } else {
-
-                            // authorization
-                            const scope = req.user.scope.split(' ');
-
-                            let meta_exec_not_found;
-
-                            if (ctx.meta[WT_META_EXEC_SCOPE]) {
-                                scope.indexOf(ctx.meta[WT_META_EXEC_SCOPE]) === -1 ? meta_exec_not_found = true : meta_exec_not_found = false;
-                            } else {
-                                meta_exec_not_found = true;
-                            }
-
-                            if (
-                                scope.indexOf(WT_META_ADMIN_SCOPE) === -1 
-                                && scope.indexOf(`wt:owner:${req.x_wt.container}`) === -1
-                                && meta_exec_not_found
-                            ) {
-                                const error = new Error('UnauthorizedError');
-                                error.statusCode = 403;
-                                return next(error);
-                            } else {
-                                return next();
-                            }
-                        }
-                    });
-
-                } catch(e){
-                    const error = new Error('UnauthorizedError');
-                    error.statusCode = 401;
-                    return next(error);
+                    if (
+                        scope.indexOf(WT_META_ADMIN_SCOPE) === -1 &&
+                        scope.indexOf(`wt:owner:${req.x_wt.container}`) ===
+                            -1 &&
+                        meta_exec_not_found
+                    ) {
+                        const error = new Error('UnauthorizedError');
+                        error.statusCode = 403;
+                        return next(error);
+                    } else {
+                        return next();
+                    }
                 }
-
-            } else {
-                const error = new Error('UnauthorizedError');
-                error.statusCode = 401;
-                return next(error);
-            }
+            });
         }
 
         // authn and authz not required
         return next();
     };
-};
+}
